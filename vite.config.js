112ad45b71efcd60530982cf2deb417e2437de40
navalnica <29257108+navalnica@users.noreply.github.com>
2023-06-21 23:26:12 +0300

    fix automatic static website build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  // following option makes built index.html use relative paths instead of absolute to link .js and .css resources.
+  // it's required for GitHub Pages.
+  base: ''
 })
